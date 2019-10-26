30bdc2c0e4832ed3da1f4750160f122ed377b158
Martin <apatino@est.frba.utn.edu.ar>
2019-10-26 18:47:44 -0300

    Task 1

---
+++ salvo/src/main/java/com/codeoftheweb/salvo/Player.java
@@ -0,0 +1,28 @@
+package com.codeoftheweb.salvo;
+
+import org.hibernate.annotations.GenericGenerator;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Player {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name="native",strategy = "native")
+    private long id;
+
+    private String userName;
+
+    public String getUserName() {
+        return userName;
+    }
+    public Player() { }
+
+    public Player(String userName) {
+        this.userName = userName;
+    }
+
+}
---
+++ salvo/src/main/java/com/codeoftheweb/salvo/SalvoApplication.java
@@ -0,0 +1,23 @@
+package com.codeoftheweb.salvo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication //SERVIDOR TOMCAT INCORPORADO
+public class SalvoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalvoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner initData(PlayerRepository playerRepository){
+		return(args) -> {
+			playerRepository.save(new Player("abraham@gmail.com"));
+
+		};
+	}
+
+}
